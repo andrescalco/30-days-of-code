ce92cb85d9d905e095f7f3d1fe58c310ef61a558
André Scalco <andre@andrescalco.com>
2018-10-03 17:47:34 -0300

    Finish fay 03 project

--- swift/03/hustleProg/hustleProg/ViewController.swift
+++ swift/03/hustleProg/hustleProg/ViewController.swift
@@ -32,24 +32,26 @@ class ViewController: UIViewController {
     let rocketView: UIView = {
         let uiview = UIView()
         
-        let cloudsImage = UIImage(named: "cloudsAndOrangeOval")
-        let cloudsImageView = UIImageView(image: cloudsImage)
-        
-        let rocketImage = UIImage(named: "rocketAndStream")
-        let rocketImageView = UIImageView(image: rocketImage)
-        
         uiview.translatesAutoresizingMaskIntoConstraints = false
         uiview.backgroundColor = UIColor(red: 79.0/255, green: 118.0/255, blue: 170.0/255, alpha: 1.0)
         
-        cloudsImageView.translatesAutoresizingMaskIntoConstraints = false
-        rocketImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        uiview.addSubview(cloudsImageView)
-        uiview.addSubview(rocketImageView)
-        
-        cloudsImageView.bottomAnchor.constraint(equalTo: uiview.bottomAnchor).isActive = true
-
         return uiview
+    }()
+    
+    let rocketImageView: UIImageView = {
+        let image = UIImage(named: "rocketAndStream")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    let cloudsImageView: UIImageView = {
+        let image = UIImage(named: "cloudsAndOrangeOval")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        return imageView
     }()
     
     override func viewDidLoad() {
@@ -72,14 +74,33 @@ class ViewController: UIViewController {
         rocketView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
         rocketView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1.0).isActive = true
         
+        rocketView.addSubview(rocketImageView)
+        rocketView.addSubview(cloudsImageView)
+        
+        cloudsImageView.bottomAnchor.constraint(equalTo: rocketView.bottomAnchor, constant: 80).isActive = true
+        cloudsImageView.leadingAnchor.constraint(equalTo: rocketView.leadingAnchor).isActive = true
+        cloudsImageView.trailingAnchor.constraint(equalTo: rocketView.trailingAnchor).isActive = true
+        
+        rocketImageView.bottomAnchor.constraint(equalTo: rocketView.bottomAnchor, constant: 300).isActive = true
+        rocketImageView.leadingAnchor.constraint(equalTo: rocketView.leadingAnchor).isActive = true
+        rocketImageView.trailingAnchor.constraint(equalTo: rocketView.trailingAnchor).isActive = true
+        
         rocketView.isHidden = true
         
     }
     
     @objc func showRocketView() {
         rocketView.isHidden = false
-    }
+        
+        self.rocketImageView.topAnchor.constraint(equalTo: self.rocketView.topAnchor, constant: 20).isActive = true
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.view.layoutIfNeeded()
+        }) { (finished) in
 
+        }
+        
+    }
 
 }
 
